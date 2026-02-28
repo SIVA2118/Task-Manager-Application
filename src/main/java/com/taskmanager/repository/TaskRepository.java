b6c372ee2b4ce1de04811169235c18e7f5dc73b4
SIVAKUMAR S <122508923+SIVA2118@users.noreply.github.com>
2026-02-28 15:38:26 +0530

    Delete src/main directory

--- src/main/java/com/taskmanager/repository/TaskRepository.java
+++
@@ -1,9 +0,0 @@
-package com.taskmanager.repository;
-
-import java.util.List;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import com.taskmanager.model.Task;
-
-public interface TaskRepository extends MongoRepository<Task, String> {
-    List<Task> findByUserId(String userId);
-}
